57f5edbfa89065cedc99aa9af724461378c28d32
Ryszard Jakielski <rlayeh@gmail.com>
2017-03-12 19:52:08 +0100

    Add basic actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,79 @@
+import { getWallet, getProducts, buy, reset } from "../api/apiConnector"
+
 export const actions = {
-  exampleAction: "EXAMPLE_ACTION"
+  insertCoin: "INSERT_COIN",
+  getWallet: "GET_WALLET",
+  getProducts: "GET_PRODUCTS",
+  returnCoins: "RETURN_COINS",
+  walletReceived: "WALLET_RECEIVED",
+  productsReceived: "PRODUCTS_RECEIVED",
+  buy: "BUY",
+  error: "ERROR"
 }
 
-
-export const exampleAction = (data) => {
+const walletReceived = wallet => {
   return {
-    type: actions.exampleAction,
-    data
+    type: actions.walletReceived,
+    wallet
+  }
+}
+
+const productsReceived = products => {
+  return {
+    type: actions.productsReceived,
+    products
+  }
+}
+
+const error = message => {
+  return {
+    type: actions.error,
+    message
+  }
+}
+
+export const insertCoin = value => {
+  return {
+    type: actions.insertCoin,
+    value
+  }
+}
+
+export const returnCoins = () => {
+  return {
+    type: actions.returnCoins
+  }
+}
+
+export const getWallet = () => {
+  return dispatch => {
+    getWallet()
+      .then(wallet => dispatch(walletReceived(wallet)))
+      .catch(exception => {
+        throw exception
+      })
+  }
+}
+
+export const getProducts = () => {
+  return dispatch => {
+    getProducts()
+      .then(products => dispatch(walletReceived(products)))
+      .catch(exception => {
+        dispatch(error(exception.message));
+      })
+  }
+}
+
+export const buy = (productId, payment) => {
+  return dispatch => {
+    buy(productId, payment)
+      .then(() => {
+        dispatch(getWallet());
+        dispatch(getProducts());
+      })
+      .catch(exception => {
+        dispatch(error(exception.message));
+      })
   }
 }
